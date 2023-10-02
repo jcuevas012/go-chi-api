53e18adc2754818e3fe079fbcb14984b7933d031
[Your Name] <[Your Work Email]>
2023-10-02 01:16:15 -0400

    [object app] Add Redis client to app struct, update Start method to handle server shutdown

--- application/app.go
+++ application/app.go
@@ -1,18 +1,23 @@
-package application
+lpackage application
 
 import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRouter(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -24,10 +29,39 @@ func (app *App) Start(ctx context.Context) error {
 		Handler: app.router,
 	}
 
-	err := server.ListenAndServe()
+	err := app.rdb.Ping(ctx).Err()
+
+	defer func() {
+		if err := app.rdb.Close(); err != nil {
+			fmt.Println("fail to close redis connection")
+		}
+	}()
 
 	if err != nil {
-		return fmt.Errorf("failed to start the server: %w ", err)
+		panic(err)
+	}
+
+	fmt.Println("Starting server app")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+
+		if err != nil {
+			ch <- fmt.Errorf("failed to start the server: %w ", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
 	}
 
 	return nil
